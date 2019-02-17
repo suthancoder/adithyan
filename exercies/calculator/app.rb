5219d175f40dc1174a8e67d157bc7caf068c94e4
Ambasuthan Jananayagam <ambasuthanjananayagam@yahoo.co.uk>
2019-02-17 09:07:29 +0000

    push multi exercise

---
+++ exercies/calculator/app.rb
@@ -0,0 +1,12 @@
+
+puts 'What numbers do you want to add?'
+
+puts "Please give me the first number"
+
+numberone = gets.to_i
+
+puts "Please give me the second number"
+
+numbertwo = gets.to_i
+
+puts "The sum of the two numbers is #{numberone+numbertwo}"
---
+++ multiplicationdrills/app.rb
@@ -0,0 +1,27 @@
+puts "READY FOR THE MULTIPLICATION RACE!"
+
+puts "READY .... STEADY .... GO!!!"
+
+  counter = 0
+20.times do
+
+  a = rand(2..11)
+  b = [3,4,5,6,9,10].sample
+  puts "#{a} X #{b}"
+  c = gets.to_i
+  if (a*b) == c
+    puts "Correct"
+    counter += 1
+  else
+    puts "Wrong"
+  end
+end
+
+  if counter/20 <= 0.6
+    puts "You got #{counter} right. Not too good ... try again!"
+  elsif counter/20 < 0.9
+    puts "You got #{counter} right. Good but room to improve"
+  else
+    puts "You got #{counter} right. Great work"
+  end
+
